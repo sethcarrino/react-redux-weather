7fa8d54d640ba3e4c0390a775549177758fcb8c6
sethcarrino <sethcarrino@gmail.com>
2017-10-05 17:13:14 -0400

    added actions - setup axios for API requests

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '85a25f700b099fbc53625a94f3f9a144';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return{
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
